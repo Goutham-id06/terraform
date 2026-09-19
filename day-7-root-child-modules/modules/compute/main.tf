3868046e971fc87108b22c6427d2d044f0e6d313
Goutham-id06 <sukkametigouthamreddy@gmail.coom>
2026-09-19 14:26:49 +0530

    day-7

--- day-7-root-child-modules/modules/compute/main.tf
+++ day-7-root-child-modules/modules/compute/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
     subnet_id = var.subnet_id
     ami = var.ami_id
     instance_type = var.instance_type
-    availability_zone = "us-east-1a"
+    availability_zone = "var.availability_zone"
     tags = {
         Name = "my_ec2"
     }
--- day-7-root-child-modules/modules/network/variables.tf
+++ day-7-root-child-modules/modules/network/variables.tf
@@ -7,3 +7,8 @@ variable "subnet_cidr" {
     type = string
     default = "null"
 }
+
+variable "availability_zone" {
+    type = string 
+    default = "us=-east-1a"
+}
