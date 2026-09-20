7a6e9a7075dc9beaba28d7695d07c11d3ea0847f
Goutham-id06 <sukkametigouthamreddy@gmail.coom>
2026-09-20 18:02:45 +0530

    calling moduels from github

---
+++ day-7-call-modules-github/main.tf
@@ -0,0 +1,8 @@
+module "network" {
+    source = "github.com/Goutham-id06/terraform/day-7-root-child-modules/modules/network"
+    vpc_cidr = "10.0.0.0/16"
+    subnet_cidr = "10.0.1.0/24"
+   
+    
+    
+}
