552a1649e2165235619a28bc592dd7401d9f1e8d
Goutham-id06 <sukkametigouthamreddy@gmail.coom>
2026-09-21 11:33:14 +0530

    upated

--- day-7-call-modules-github/main.tf
+++ day-7-call-modules-github/main.tf
@@ -2,7 +2,4 @@ module "network" {
     source = "github.com/Goutham-id06/terraform/day-7-root-child-modules/modules/network"
     vpc_cidr = "10.0.0.0/16"
     subnet_cidr = "10.0.1.0/24"
-   
-    
-    
 }
